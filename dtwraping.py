4f668af5f03ad6f94fbc79ed4fa08dc79fd28414
M4rt11 <64250241+M4rt11@users.noreply.github.com>
2022-11-29 15:19:26 +0100

    dtw manual

--- dtwraping.py
+++ dtwraping.py
@@ -9,12 +9,12 @@ df2 = pd.read_csv('Domain1_csv/Subject1-0-2.csv')
 print(df1.head())
 
 
-x1 = np.array(df1.iloc[:,0])
-y1 = np.array(df1.iloc[:,1])
+x1 = np.array(df1.iloc[:20,0])
+y1 = np.array(df1.iloc[:20,1])
 
 x2 = np.array(df2.iloc[:,0])
 y2 = np.array(df2.iloc[:,1])
-
+'''
 
 plt.plot(x1,y1)
 plt.plot(x2,y2)
@@ -41,3 +41,51 @@ print(d)
 plt.imshow(acc_cost_matrix.T, origin='lower', cmap='gray', interpolation='nearest')
 plt.plot(path[0], path[1], 'w')
 plt.show()
+'''
+### remplacer le module DTW par du code ##
+
+## https://towardsdatascience.com/dynamic-time-warping-3933f25fcdd
+
+def dtw(s, t):
+    n, m = len(s), len(t)
+    dtw_matrix = np.zeros((n + 1, m + 1))
+    for i in range(n + 1):
+        for j in range(m + 1):
+            dtw_matrix[i, j] = np.inf
+    dtw_matrix[0, 0] = 0
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            cost = abs(s[i - 1] - t[j - 1])
+            # take last min from a square box
+            last_min = np.min([dtw_matrix[i - 1, j], dtw_matrix[i, j - 1], dtw_matrix[i - 1, j - 1]])
+            dtw_matrix[i, j] = cost + last_min
+    return dtw_matrix
+
+print(dtw(x1, x2))
+print("----------------------------------------------------")
+
+
+def dtw(s, t, window):
+    n, m = len(s), len(t)
+    w = np.max([window, abs(n - m)])
+    dtw_matrix = np.zeros((n + 1, m + 1))
+
+    for i in range(n + 1):
+        for j in range(m + 1):
+            dtw_matrix[i, j] = np.inf
+    dtw_matrix[0, 0] = 0
+
+    for i in range(1, n + 1):
+        for j in range(np.max([1, i - w]), np.min([m, i + w]) + 1):
+            dtw_matrix[i, j] = 0
+
+    for i in range(1, n + 1):
+        for j in range(np.max([1, i - w]), np.min([m, i + w]) + 1):
+            cost = abs(s[i - 1] - t[j - 1])
+            # take last min from a square box
+            last_min = np.min([dtw_matrix[i - 1, j], dtw_matrix[i, j - 1], dtw_matrix[i - 1, j - 1]])
+            dtw_matrix[i, j] = cost + last_min
+    return dtw_matrix
+
+print(dtw(x1, x2, window = 3))
