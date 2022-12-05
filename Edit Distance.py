bd0f34a1bd4b25f92c6ce49842d70ae0ba7ef2c8
axellelechien <axelle.lechien@student.uclouvain.be>
2022-12-05 18:26:58 +0100

    hihi
    hih

--- Edit Distance.py
+++ Edit Distance.py
@@ -14,15 +14,8 @@ def standarization(data):
     data.iloc[:, 0:2] = scaler.fit_transform(data.iloc[:, 0:2]) #que le x, y, z et pas le t
     return data
 
-#CREATION DU TABLEAU
 
-  # On crée une matrice de 0 (2 colonnes et 1000 lignes)
-data = np.zeros((1000, 2)) # (Colonnes : 1 = subject, 2 = number qu'il fait)
-for i in range(1000):
-    data[i] = [int((i) // 100 + 1), int((i) // 10) % 10]
-data = pd.DataFrame(data, columns=['UserID', 'Digit'])
-
- #On crée une liste de coordonnées x et y pour chaque répétition, liste coord [[x],[y]]
+### liste de coordonées de point x, y
 coord = []
 for subject in range(1,11):
     for digit in range(10):
@@ -30,14 +23,22 @@ for subject in range(1,11):
             f_name = f"Domain1_csv/Subject{subject}-{digit}-{rep}.csv"
             df = pd.read_csv(f_name)
             standarization(df)
-            x, y, z = np.array(df.iloc[3:, 0]), np.array(df.iloc[3:, 1]), np.array(df.iloc[3:, 2])
-            x5 = gaussian_filter1d(x, sigma=5) #on filtre les points
-            y5 = gaussian_filter1d(y, sigma=5)
-            z5 = gaussian_filter1d(z, sigma=5)
-            coord.append(np.array([x5, y5]))
-coord = pd.Series(coord) #on dit que les coord sont égales à un dataframe
-data["Coord"] = coord #on crée un nouvel index coord dans la matrice data, on rajoute une colonne
+            #print(df)
+            for coord_df in range(len(df)):
+                coord.append([df.iloc[coord_df,0], df.iloc[coord_df,1]])
+            #print(coord)
 
+coord = pd.DataFrame(coord)
+#print(coord.iloc[:,0])
+#plt.scatter(coord.iloc[:, 0], coord.iloc[ :, 1], s = 50, c='b')
+plt.show()
 
-kmeans = KMeans(init="random", n_init=10, max_iter=300, random_state=42)
-kmeans.fit(data["Coord"])
+###CLUSTERING
+kmeans = KMeans(init="random", n_init=10, max_iter=300, random_state=42, n_clusters=20)
+kmeans.fit(coord.iloc[:, 0:2])
+y_kmeans = kmeans.predict(coord.iloc[:, 0:2])
+plt.scatter(coord.iloc[:, 0], coord.iloc[:,1], c=y_kmeans)
+centers = kmeans.cluster_centers_
+print(kmeans.cluster_centers_)
+plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+plt.show()
