47c7595f8d415b819bdc606e710a380e843d2a3f
M4rt11 <64250241+M4rt11@users.noreply.github.com>
2022-12-10 12:32:32 +0100

    train / test

--- martineditdistance.py
+++ martineditdistance.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,13 +7,15 @@ from sklearn.preprocessing import StandardScaler
 from scipy import stats as st
 from sklearn.cluster import KMeans
 
-#STANDARDISATION
+
+# STANDARDISATION
 def standarization(data):
     scaler = StandardScaler()
-    data.iloc[:, 0:2] = scaler.fit_transform(data.iloc[:, 0:2]) #que le x, y, z et pas le t
+    data.iloc[:, 0:2] = scaler.fit_transform(data.iloc[:, 0:2])  # que le x, y, z et pas le t
     return data
 
 
+"""
 ### liste de coordonées de point x, y
 coord = []
 for subject in range(1,11):
@@ -28,10 +29,37 @@ for subject in range(1,11):
             for coord_df in range(len(df)):
                 coord.append([int(numb),df.iloc[coord_df,0], df.iloc[coord_df,1]])
 
+                """
+### Train and Test
+coord_train = []
+coord_test = []
+###Train
+x = 5
+for subject in range(1, 11):
+    for digit in range(10):
+        for rep in range(1, 11):
+            if subject != x:
+                f_name = f"Domain1_csv/Subject{subject}-{digit}-{rep}.csv"
+                df = pd.read_csv(f_name)
+                standarization(df)
+                # print(df)
+                numb = str(subject) + str(digit) + str(rep)
+                for coord_df in range(len(df)):
+                    coord_train.append([int(numb), df.iloc[coord_df, 0], df.iloc[coord_df, 1]])
+            else:
+                f_name = f"Domain1_csv/Subject{subject}-{digit}-{rep}.csv"
+                df = pd.read_csv(f_name)
+                standarization(df)
+                # print(df)
+                numb = str(subject) + str(digit) + str(rep)
+                for coord_df in range(len(df)):
+                    coord_test.append([int(numb), df.iloc[coord_df, 0], df.iloc[coord_df, 1]])
 
-coord = pd.DataFrame(coord)
-#print(coord.iloc[:,0])
-#plt.scatter(coord.iloc[:, 0], coord.iloc[ :, 1], s = 50, c='b')
+coord_train, coord_test = pd.DataFrame(coord_train), pd.DataFrame(coord_test)
+print('train : ', coord_train)
+print('test : ', coord_test)
+# print(coord.iloc[:,0])
+# plt.scatter(coord.iloc[:, 0], coord.iloc[ :, 1], s = 50, c='b')
 plt.show()
 
 ###CLUSTERING
@@ -51,40 +79,37 @@ print(type(coord))
 ###Créer tableau avec les séquences des labels
 
 ck = []
-string=[]
+string = []
 
-for i in range(len(coord)-1):
-    if coord.iloc[i,0] == coord.iloc[i+1,0]:
-        string.append(coord.iloc[i,-1])
+for i in range(len(coord) - 1):
+    if coord.iloc[i, 0] == coord.iloc[i + 1, 0]:
+        string.append(coord.iloc[i, -1])
     else:
         ck.append(string)
         string = []
 
 print(ck)
 
-
-
 ###Supprimer les repetitions
 ck_new = []
 for i in range(len(ck)):
-    newstring=[]
+    newstring = []
     for indice in range(len(ck[i])):
-        if indice == len(ck[i])-1:
+        if indice == len(ck[i]) - 1:
             newstring.append(ck[i][-1])
-            #print('finish')
-        elif ck[i][indice] != ck[i][indice+1]:
+            # print('finish')
+        elif ck[i][indice] != ck[i][indice + 1]:
             newstring.append(ck[i][indice])
-        elif ck[i][indice] == ck[i][indice+1]:
-               pass
+        elif ck[i][indice] == ck[i][indice + 1]:
+            pass
     ck_new.append(newstring)
 
 print(ck_new)
 print(type(ck_new))
 
-"""
-#Definition edit distance
-def edit_distance(stringtest, stringtrain):
 
+# Definition edit distance
+def edit_distance(stringtest, stringtrain):
     if len(stringtest) > len(stringtrain):
         difference = len(stringtest) - len(stringtrain)
         stringtest = stringtest[:difference]
@@ -102,7 +127,7 @@ def edit_distance(stringtest, stringtrain):
 
     return difference
 
+
 print(edit_distance(ck_new[0], ck_new[32]))
 
-"""
-#Retourner la difference minimale
+# Retourner la difference minimale
